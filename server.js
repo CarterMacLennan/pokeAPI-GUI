d594c723e2c89e28e8c3e07e80c1dcf28071a332
Carter MacLennan <carter.maclennan@verosource.com>
2020-11-28 17:27:49 -0400

    fixed folder path for serving favicon

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ const app = express();
 app.use(logger("dev"));
 app.use(express.json());
 app.use(favicon(__dirname + '/../client/build/favicon.ico'));
-app.use(express.static(path.join(__dirname, '/../client/build')));
+app.use(express.static(path.join(__dirname, '/../client/public')));
 
 if (process.env.NODE_ENV === 'production') {
     app.get('/*', function(req, res) {
-        res.sendFile(path.join(__dirname, '/../client/build', "index.html"));
+        res.sendFile(path.join(__dirname, '/../client/public', "index.html"));
     });
     
     const port = process.env.PORT || 5000;
