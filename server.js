3b55345555c1e7866c2416e7ed6e540f49de92ec
Carter MacLennan <carter.maclennan@verosource.com>
2020-11-29 17:09:25 -0400

    Updated scripts for local development

--- server.js
+++ server.js
@@ -2,15 +2,14 @@ const path = require('path'),
     express = require('express'),
     favicon = require("serve-favicon"),
     mongoose = require("mongoose");
-    
-const app = express();
-
-app.use(express.json());
-
-app.use(favicon(__dirname + '/client/public/favicon.ico'));
-app.use(express.static(path.join(__dirname, '/client/build')));
 
 const db = process.env.DB_URI || "mongodb://localhost:27017/pokeGUI";
+const port = process.env.PORT || 5000;
+
+const app = express();
+app.use(express.json());
+app.use(favicon(__dirname + '/client/public/favicon.ico'));
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 mongoose.connect( db, {useNewUrlParser: true ,useUnifiedTopology: true }, (err) => {
     if (err) {
@@ -27,4 +26,4 @@ const userSchema = new mongoose.Schema ({
 
 const User = new mongoose.model("User", userSchema);
 
-app.listen(process.env.PORT || 5000, () => console.log(`Server now running`));
+app.listen( port, () => console.log(`Server now running`));
