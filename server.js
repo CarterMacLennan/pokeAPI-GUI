ce11e55965eb3448c5838e1c341ba560e8443a6f
Carter MacLennan <carter.maclennan@verosource.com>
2020-11-29 16:28:54 -0400

    Local build runs mongoDB locally, runs MongoAtlas while on Heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const path = require('path'),
     express = require('express'),
-    favicon = require("serve-favicon");
+    favicon = require("serve-favicon"),
+    mongoose = require("mongoose");
     
 const app = express();
 
@@ -8,6 +9,23 @@ app.use(express.json());
 
 app.use(favicon(__dirname + '/client/public/favicon.ico'));
 app.use(express.static(path.join(__dirname, '/client/build')));
+
+const db = process.env.DB_URI || "mongodb://localhost:27017/pokeGUI";
+
+mongoose.connect( db, {useNewUrlParser: true ,useUnifiedTopology: true }, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Connected to MongoDB...");
+    }
+});
+
+const userSchema = new mongoose.Schema ({
+    email: String,
+    password: String,
+});
+
+const User = new mongoose.model("User", userSchema);
 
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, '/client/build', "index.html"));
