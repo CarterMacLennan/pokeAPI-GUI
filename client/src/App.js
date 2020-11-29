81c78b5bc5638f805b1cb4b3749ace2d5d96a057
Carter MacLennan <carter.maclennan@verosource.com>
2020-11-29 18:26:33 -0400

    Added Sign in with google button

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
+import './styles/bootstrap-social.css';
 import './styles/styles.css';
 
 export default function App() {
   return (
-    <div className="App">Hello World</div>
+  <div className="App">
+    <button class="btn btn-md btn-social btn-google">
+      <span class="fa fa-google"></span> Sign in with Google
+    </button>
+  </div>
   );
 }
