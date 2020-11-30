25d588a74db427bd9364cea0975afb163c5d144d
Carter MacLennan <carter.maclennan@verosource.com>
2020-11-29 21:45:47 -0400

    reverted back to 04393ba, will not be using google auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,7 @@
-import './styles/bootstrap-social.css';
 import './styles/styles.css';
 
 export default function App() {
   return (
-  <div className="App">
-    <button class="btn btn-md btn-social btn-google">
-      <span class="fa fa-google"></span> Sign in with Google
-    </button>
-  </div>
+    <div className="App">Hello World</div>
   );
 }
