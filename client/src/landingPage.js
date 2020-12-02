c5e77e4565918f388c24b575dedd5f20728dd49c
Carter MacLennan <carter.maclennan@verosource.com>
2020-12-01 21:53:21 -0400

    Landing page pikachu image & search bar now resizes automatically

--- client/src/landingPage.js
+++ client/src/landingPage.js
@@ -10,7 +10,7 @@ export default class LandingPage extends React.Component{
         return (
             <div>
                 <form className="justify-content-center md-form form-inline main-search">
-                    <img src={Pikachu} height="175px" width="175px"/>
+                    <img src={Pikachu} className="img-pikachu"/>
                     <input className="form-control form-control-lg w-50" type="text" placeholder="Search" aria-label="Search"/>
                     <i className="fas fa-search" aria-hidden="true"></i>
                 </form>
