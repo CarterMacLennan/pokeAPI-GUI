17e71fb3150221aa4f96cf528ba9c677919adad4
Carter MacLennan <carter.maclennan@verosource.com>
2020-12-02 01:05:36 -0400

    Changed anchor tag to Link for proper routing to login component

--- client/src/favourites.js
+++ client/src/favourites.js
@@ -1,26 +1,21 @@
 import React from "react";
 import Heart from "./images/heart.png";
+import {Link} from "react-router-dom";
 
-export default class Favourites extends React.Component{
-    constructor(props){
-        super(props);
-    }
-
-    render() {
-        return (
-            <div className="container">
-                <div className="row justify-content-center">
-                    <div className="auth-container col-xl-5 col-sm-6">
-                        <div className="account-wall">
-                            <img className="heart-img" src={Heart} alt="Heart"/>
-                            <p>Create an account to save your favourite Pokemon!</p>
-                            <form className="form-login form-block">
-                                <a href="/login" className="btn btn-lg btn-danger btn-block btn-login" type="submit">Create an Account</a>
-                            </form>
-                        </div>
+export default function Favourites() {
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="auth-container col-xl-5 col-sm-6">
+                    <div className="account-wall">
+                        <img className="heart-img" src={Heart} alt="Heart"/>
+                        <p>Create an account to save your favourite Pokemon!</p>
+                        <form className="form-login form-block">
+                            <Link to="/login"><button className="btn btn-lg btn-danger btn-block btn-login">Create an Account</button></Link>
+                        </form>
                     </div>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
