015a20fc1ff5362983bcef1076e09d836a4fd9e5
Carter MacLennan <carter.maclennan@verosource.com>
2020-12-01 22:58:30 -0400

    Added react router to route / to landing page & set navbar as a wrapper class

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import './styles/styles.css';
 import Navigation from './navigation'
 import LandingPage from './landingPage';
+import { Route, Switch } from 'react-router-dom';
 
 export default function App() {
   return (
     <div className="App">
       <Navigation />
-      <LandingPage />
+      <Switch>
+          <Route path="/" component={LandingPage} exact />
+      </Switch>
     </div>
   );
 }
--- client/src/navigation.js
+++ client/src/navigation.js
@@ -14,18 +14,15 @@ export default class Navigation extends React.Component{
             </button>
             <div className="collapse navbar-collapse navbar-dark" id="navbarNav">
                 <ul className="navbar-nav">
-                <li className="nav-item active">
-                    <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Login</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link disabled" href="#">Favourites</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link disabled" href="#">Search History</a>
-                </li>
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Login</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link disabled" href="#">Favourites</a>
+                    </li>
                 </ul>
             </div>
         </nav>
