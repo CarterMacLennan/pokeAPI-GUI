bc50816496705ecafab7f73979640d7cbc91a22f
Carter MacLennan <carter.maclennan@verosource.com>
2020-12-02 19:26:13 -0400

    Added jumbotron to landing page

--- client/src/landingPage.js
+++ client/src/landingPage.js
@@ -9,16 +9,24 @@ export default class LandingPage extends React.Component{
     render() {
         return (
             <div>
-                <form className="row md-form form-inline main-search">
-                    <div className="col-1"/>
-                    <div className="col-2">
-                        <img src={Pikachu} className="img-pikachu"/>
+                <div className="jumbotron jumbotron-fluid">
+                    <div className="container">
+                        <div className="row">
+                            <div className="header-item col-2">
+                                <img src={Pikachu} className="img-pikachu"/>
+                            </div>
+                            <div className="col-1"/>
+                            <div className="header-item col-9">
+                                <h1 class="display-6">PokeGui</h1>
+                                <p class="lead">A simple GUI to help you navigate the popular PokeAPI.</p>
+                            </div>
+                        </div>
                     </div>
-                    <div className="col-1"/>
-                    <div className="col-7">
+                </div>
+                <form className="row md-form justify-content-center form-inline main-search">
+                    <div className=" col-lg-8 col-sm-9 col-10">
                         <input className="form-control form-control-lg w-100" type="text" placeholder="Search" aria-label="Search"/>
                     </div>   
-                    <div className="col-1"/>
                 </form>
             </div>
         );
--- client/src/navigation.js
+++ client/src/navigation.js
@@ -7,8 +7,7 @@ export default class Navigation extends React.Component{
 
     render() {
         return (
-            <nav className="navbar navbar-expand-lg navbar-dark bg-danger">
-            <a className="navbar-brand" href="/"><h4>PokeGUI</h4></a>
+            <nav className="navbar navbar-expand-lg navbar-dark">
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
