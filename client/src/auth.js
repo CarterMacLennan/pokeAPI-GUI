23ec6398b74459a0f83171a9df675b4dbc990dc4
Carter MacLennan <carter.maclennan@verosource.com>
2020-12-02 00:09:37 -0400

    Authentication screen now responds better on tablets & refactored css for readability

--- client/src/auth.js
+++ client/src/auth.js
@@ -8,15 +8,15 @@ export default class Navigation extends React.Component{
 
     render() {
         return (
-            <div className="container auth-container">
+            <div className="container">
                 <div className="row justify-content-center">
-                    <div className="col-sm-6 col-md-4 col-md-offset-4">
+                    <div className="auth-container col-md-offset-4 col-md-6 col-sm-6">
                         <div className="account-wall">
                             <img className="profile-img" src={Bullbasaur} alt="Bullbasaur"/>
-                            <form className="form-login">
+                            <form className="form-login form-block">
                                 <input type="text" className="form-control" placeholder="Email" required autofocus/>
                                 <input type="password" className="form-control" placeholder="Password" required/>
-                                <button className="btn btn-lg btn-success btn-block" type="submit">Sign in</button>
+                                <button className="btn btn-lg btn-success btn-block btn-login" type="submit">Sign in</button>
                             </form>
                         </div>
                     </div>
