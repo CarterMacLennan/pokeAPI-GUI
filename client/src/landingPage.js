f8bb52e5bac9de6e8ae343e83abbfa18beffff67
Carter MacLennan <carter.maclennan@verosource.com>
2020-12-01 22:07:19 -0400

    Removed search icon & made search bar margin-top dynamic

--- client/src/landingPage.js
+++ client/src/landingPage.js
@@ -12,7 +12,6 @@ export default class LandingPage extends React.Component{
                 <form className="justify-content-center md-form form-inline main-search">
                     <img src={Pikachu} className="img-pikachu"/>
                     <input className="form-control form-control-lg w-50" type="text" placeholder="Search" aria-label="Search"/>
-                    <i className="fas fa-search" aria-hidden="true"></i>
                 </form>
             </div>
         );
