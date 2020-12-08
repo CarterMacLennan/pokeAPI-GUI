376d1318c9f72dc4956c3f016b654825b7e41af8
Carter MacLennan <carter.maclennan@verosource.com>
2020-12-07 22:17:15 -0400

    Added password hashing & salting upon registration & authentication for login

--- client/src/auth.js
+++ client/src/auth.js
@@ -1,9 +1,31 @@
 import React from "react";
 import Bullbasaur from "./images/bullbasaur.png";
+import axios from "axios";
+import qs from "query-string";
 
 export default class Navigation extends React.Component{
     constructor(props){
         super(props);
+
+        this.state = {
+            username: "",
+            password: "",
+        }
+    }
+
+    handleRegistration = (e) => {
+        e.preventDefault();
+        console.log(this.state);
+        axios({
+            method: "post",
+            url: "/api/login",
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}, 
+            data: qs.stringify({
+                username: this.state.username, 
+                password: this.state.password,
+            })
+        })
+        .then(res => console.log(res));
     }
 
     render() {
@@ -15,9 +37,9 @@ export default class Navigation extends React.Component{
                             <img className="bullbasaur-img" src={Bullbasaur} alt="Bullbasaur"/>
                             <p>Login to save your favourite Pokemon!</p>
                             <form className="form-login form-block">
-                                <input type="text" className="form-control" placeholder="Email" required autofocus/>
-                                <input type="password" className="form-control" placeholder="Password" required/>
-                                <button className="btn btn-lg btn-success btn-block btn-login" type="submit">Login</button>
+                                <input type="text" value={this.state.username} onChange={e => this.setState({username: e.target.value})} className="form-control" placeholder="Email" required autoFocus/>
+                                <input type="password" value={this.state.password} onChange={e => this.setState({password: e.target.value})} className="form-control" placeholder="Password" required/>
+                                <button className="btn btn-lg btn-success btn-block btn-login" onClick={this.handleRegistration}>Login</button>
                             </form>
                         </div>
                     </div>
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const path = require('path'),
     express = require('express'),
     favicon = require("serve-favicon"),
-    mongoose = require("mongoose");
+    mongoose = require("mongoose"),
+    bodyParser = require("body-parser"),
+    bcrypt = require("bcrypt"), 
+    saltRounds = 10;
 
 const db = process.env.DB_URI || "mongodb://localhost:27017/pokeGUI";
 const port = process.env.PORT || 5000;
 
 const app = express();
+app.use(express.urlencoded());
 app.use(express.json());
 
 mongoose.connect( db, {useNewUrlParser: true ,useUnifiedTopology: true }, (err) => {
@@ -23,6 +27,51 @@ const userSchema = new mongoose.Schema ({
 });
 
 const User = new mongoose.model("User", userSchema);
+
+app.post("/api/register", async (req,res) => {
+    console.log([req.body.username, req.body.password]);
+
+    bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save( error => {
+            if (error){
+                console.log(error);
+                res.send(error);
+            }
+            else {
+                console.log("true");
+                res.send(true);
+            }
+        });
+    });
+});
+
+app.post("/api/login", (req, res) => {
+    User.findOne({email: req.body.username}, (err, user) => {
+        if (err) {
+            console.log(err);
+        }
+        else if (user){
+            bcrypt.compare(req.body.password, user.password, (error, result) => {
+                if (result) {
+                    console.log("Valid!");
+                    res.send("Valid!");
+                }
+                else {
+                    console.log("Invalid Password");
+                    res.send("Invalid Password");
+                }
+            });
+        }
+        else {
+            console.log("Invalid UserName");
+            res.send("Invalid UserName");
+        }
+    })
+});
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
